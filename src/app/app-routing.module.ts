c23acc8e6e36adb0184b2de03629a0fba5205c53
Jefferson H <210017@upf.br>
2025-10-29 20:58:07 -0300

    primeira modificação - Jeff

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'login',
+    redirectTo: 'home',
     pathMatch: 'full'
   },
-  {
-    path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule)
-  },
+
+  //{
+ //   path: 'login',
+  //  loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule)
+  //},
+  
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
